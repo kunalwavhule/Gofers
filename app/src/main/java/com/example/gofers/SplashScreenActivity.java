f79f5caec8fe7b7bc8532f6bb0c73c82b7f9fe12
kanishk <kanishk.dubeyh.bad@gmail.com>
2022-05-05 11:01:54 +0530

    sign up screen added

--- app/src/main/java/com/example/gofers/SplashScreenActivity.java
+++ app/src/main/java/com/example/gofers/SplashScreenActivity.java
@@ -16,7 +16,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(i);
                 finish();
             }
